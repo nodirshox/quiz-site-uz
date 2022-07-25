485c70db99aef863eeca9b61156e8a7a82e8aad4
Nodirbek Ergashev <mr.nodirbek77@gmail.com>
2022-07-25 10:18:18 -0500

    feat: Calculation of result

--- quiz.js
+++ quiz.js
@@ -5,16 +5,33 @@ const questionNumber = parseInt(params.get("q"));
 let totalQuestions = 0;
 
 window.onload = function() {
-    // Identify page
-    const path = location.pathname;
-    const currentPage = path.split("/").pop();
+    try {
+        // Identify page
+        const path = location.pathname;
+        const currentPage = path.split("/").pop();
 
-    if (currentPage.length === 0 || currentPage === "index.html") {
-        populateSubjects();
-    } else if (currentPage === "quiz.html") {
-        // Add event listener to submit button
-        populateQuestion();
-        document.getElementById("submit").addEventListener("click", answerQuestion);
+        if (currentPage.length === 0 || currentPage === "index.html") {
+            populateSubjects();
+        } else if (currentPage === "quiz.html") {
+            // Add event listener to submit button
+            populateQuestion();
+            document.getElementById("submit").addEventListener("click", answerQuestion);
+        } else if (currentPage === "result.html") {
+            // Search subject by query param
+            const subject = searchSubjectById(subjectId);
+
+            // Set subject to h1
+            document.getElementById("subject").innerText = subject.title + " Quiz"; 
+            totalQuestions = countTotalQuestion(subject.questions);     
+            const correctAnswers = countAnswers(subjectId);
+            document.getElementById("correct").innerText = `${correctAnswers} of ${totalQuestions}`;
+            
+            const percentage = ((correctAnswers / totalQuestions) * 100).toFixed(0);
+            document.getElementById("percentage").innerText = `${percentage}%`;
+        }
+    } catch (e) {
+        alert(e);
+        window.location = "index.html";
     }
 }
 
@@ -36,13 +53,13 @@ function answerQuestion() {
         if (history == null) {
             history = questionNumber + "-" + selectedSize;
         } else {
-            history = history + ", " + questionNumber + "-" + selectedSize;
+            history = history + "," + questionNumber + "-" + selectedSize;
         }
         localStorage.setItem(subjectId, history);
 
         // check if it is final question otherwise redirect to next question
         if (questionNumber === totalQuestions) {
-            window.location = "result.html";
+            window.location = `result.html?id=${subjectId}`;
         } else {
             window.location = `quiz.html?id=${subjectId}&q=${questionNumber + 1}`;
         }
@@ -63,50 +80,78 @@ function populateSubjects() {
 }
 
 function populateQuestion() {
-    try {        
-        // Search subject by query param
-        const subjects = data.filter(e => e.id == subjectId);
-        if (subjects.length !== 1) throw "Subject not found";
-    
-        // Set subject to h1
-        const subjectsTitle = subjects[0].title;
-        document.getElementById("subject").innerText = subjectsTitle + " Quiz";
-    
-        // check question is empty or not
-        const questions = subjects[0].questions;
-        if (questions.length === 0) throw "Question set is empty, come back later..."
-        totalQuestions = questions.length;
+    // Search subject by query param
+    const subject = searchSubjectById(subjectId);
 
-        // Find question by id
-        const selectedQuestion = questions.filter(e => e.id == questionNumber);
-        if (selectedQuestion.length !== 1) throw "Question not found";
-    
-        // Set current question number
-        document.getElementById("current").innerHTML = `Question ${questionNumber} of ${totalQuestions}`;
+    // Set subject to h1
+    document.getElementById("subject").innerText = subject.title + " Quiz";
 
-        // Delete localStorage if question number 1
-        if (questionNumber === 1) {
-            localStorage.removeItem(subjectId);
-        }
+    totalQuestions = countTotalQuestion(subject.questions);
 
-        // Set question
-        document.getElementById("title").innerText = selectedQuestion[0].body;
-    
-        // Populate questions
-        const options = selectedQuestion[0].options.map((e, index) => {
-            return `
-            <label for="answer${index + 1}" class="option">
-                <input type="radio" id="answer${index + 1}" value=${e.id} name="question">
-                ${e.text}
-            </label>
-            `
-        }).join("");
-        document.getElementById("options").insertAdjacentHTML("beforeend", options);
+    // Find question by id
+    const selectedQuestion = subject.questions.filter(e => e.id == questionNumber);
+    if (selectedQuestion.length !== 1) throw "Question not found";
 
-    } catch (e) {
-        alert(e);
-        window.location = "index.html";
+    // Set current question number
+    document.getElementById("current").innerHTML = `Question ${questionNumber} of ${totalQuestions}`;
+
+    // Delete localStorage if question number 1
+    if (questionNumber === 1) {
+        localStorage.removeItem(subjectId);
     }
+
+    // Set question
+    document.getElementById("title").innerText = selectedQuestion[0].body;
+
+    // Populate questions
+    const options = selectedQuestion[0].options.map((e, index) => {
+        return `
+        <label for="answer${index + 1}" class="option">
+            <input type="radio" id="answer${index + 1}" value=${e.id} name="question">
+            ${e.text}
+        </label>
+        `
+    }).join("");
+    document.getElementById("options").insertAdjacentHTML("beforeend", options);
+}
+
+function searchSubjectById(subjectId) {
+    const subjects = data.filter(e => e.id == subjectId);
+    if (subjects.length !== 1) throw "Subject not found";
+    return subjects[0];
+}
+
+function countTotalQuestion(questions) {
+    // Check question is empty or not
+    if (questions.length === 0) throw "Question set is empty, come back later..."
+    return questions.length;
+}
+
+function countAnswers(subjectId) {
+    let answers = localStorage.getItem(subjectId);
+    if (answers == null) {
+        throw "Answer set not found";
+    }
+    answers = answers.split(",").map(e => {
+        return {
+            question_id: e[0],
+            answer_id: e[2]
+        }
+    });
+    const subject = searchSubjectById(subjectId);
+    const questions = subject.questions;
+    let total = 0;
+    for (let i = 0; i < questions.length; i++) {
+        let correctAnswer = questions[i].options.filter(e => e.correct == true)[0];
+        for (let j = 0; j < answers.length; j++) {
+            if (
+                answers[j].question_id == questions[i].id &&
+                answers[j].answer_id == correctAnswer.id) {
+                total++;
+            }
+        }
+    }
+    return total;
 }
 
 // Questions
@@ -117,42 +162,53 @@ const data = [
         "questions": [
             {
                 "id": "1",
-                "body": "What is PHP?",
+                "body": "What is HTML?",
                 "options": [
                     {
                         "id": "1",
-                        "text": "Elephant",
-                        "correct": false
-                    },
-                    {
-                        "id": "2",
-                        "text": "Programming language",
+                        "text": "HyperText Markup Language",
                         "correct": true
                     },
                     {
-                        "id": "3",
-                        "text": "Car",
+                        "id": "2",
+                        "text": "Personal home page",
                         "correct": false
                     }
                 ]
             },
             {
                 "id": "2",
-                "body": "How to declare variable in PHP?",
+                "body": "What is <b> tag?",
                 "options": [
                     {
                         "id": "1",
-                        "text": "Elephant",
+                        "text": "Emphasized text",
                         "correct": false
                     },
                     {
                         "id": "2",
-                        "text": "Programming language",
+                        "text": "Make text bol",
                         "correct": true
                     },
                     {
                         "id": "3",
-                        "text": "Car",
+                        "text": "Underline text",
+                        "correct": false
+                    }
+                ]
+            },
+            {
+                "id": "3",
+                "body": "What is WWW?",
+                "options": [
+                    {
+                        "id": "1",
+                        "text": "World wide web",
+                        "correct": true
+                    },
+                    {
+                        "id": "2",
+                        "text": "Spider",
                         "correct": false
                     }
                 ]
@@ -162,16 +218,6 @@ const data = [
     {
         "id": "2",
         "title": "CSS",
-        "questions": []
-    },
-    {
-        "id": "3",
-        "title": "jQuery",
-        "questions": []
-    },
-    {
-        "id": "4",
-        "title": "NodeJS",
         "questions": []
     }
 ]
