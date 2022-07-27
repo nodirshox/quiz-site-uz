c7bd17221f311eb16324418ab2193cf1d4127bbb
Nodirbek Ergashev <mr.nodirbek77@gmail.com>
2022-07-27 08:51:26 -0500

    feat: Question data separated

---
+++ questions.js
@@ -0,0 +1,82 @@
+// Questions
+const data = [
+    {
+        "id": "1",
+        "title": "HTML",
+        "questions": [
+            {
+                "id": "1",
+                "body": "What is the full form of HTML?",
+                "options": [
+                    {
+                        "id": "1",
+                        "text": "HyperText Markup Language",
+                        "correct": true
+                    },
+                    {
+                        "id": "2",
+                        "text": "Personal home page",
+                        "correct": false
+                    }
+                ]
+            },
+            {
+                "id": "2",
+                "body": "What is <b> tag?",
+                "options": [
+                    {
+                        "id": "1",
+                        "text": "Emphasized text",
+                        "correct": false
+                    },
+                    {
+                        "id": "2",
+                        "text": "Make text bol",
+                        "correct": true
+                    },
+                    {
+                        "id": "3",
+                        "text": "Underline text",
+                        "correct": false
+                    }
+                ]
+            },
+            {
+                "id": "3",
+                "body": "What is WWW?",
+                "options": [
+                    {
+                        "id": "1",
+                        "text": "World wide web",
+                        "correct": true
+                    },
+                    {
+                        "id": "2",
+                        "text": "Spider",
+                        "correct": false
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "id": "2",
+        "title": "CSS",
+        "questions": [{
+                "id": "1",
+                "body": "What is the full form of CSS?",
+                "options": [
+                    {
+                        "id": "1",
+                        "text": "Style Web Pages",
+                        "correct": false
+                    },
+                    {
+                        "id": "2",
+                        "text": "Cascading Style Sheets",
+                        "correct": true
+                    }
+                ]
+        }]
+    }
+]
